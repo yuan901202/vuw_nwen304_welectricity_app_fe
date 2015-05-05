7fa58b3ddfe8e158be76ed76008989b1cb51e858
John Armstrong <j.armstrong484@gmail.com>
2015-05-05 16:41:03 +1200

    Added some defaults to gameService. GameService can now add and remove power plants

--- www/js/services/gameService.js
+++ www/js/services/gameService.js
@@ -4,6 +4,20 @@
 angular.module('welc.services', [])
     .service('GameService', [function () {
         var running = false;
+        var pollution = 0;  //Should be between 0 - 100
+        var population = 100000; //Set initial population to 100, 000
+        var powerPlants = []; //Contains all the power plants in the game
+
+        this.addPowerPlant = function (powerPlant) {
+            powerPlants.push(powerPlant);
+        };
+
+        this.removePowerPlant = function (powerPlant) {
+            var powerPlantIndex = powerPlant.indexOf(powerPlant);
+            if (powerPlantIndex > -1) {
+                powerPlants.splice(powerPlantIndex, 1);
+            }
+        }
 
         this.start = function () {
             running = true;
