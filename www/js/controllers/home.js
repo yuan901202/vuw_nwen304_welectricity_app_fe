8f076b459bbc1a3efea0c9e0f34d7694f28d59cf
John Armstrong <j.armstrong484@gmail.com>
2015-05-05 20:00:08 +1200

    Game service will now run some logic every X milliseconds when started and will stop running when paused.

--- www/js/controllers/home.js
+++ www/js/controllers/home.js
@@ -1,6 +1,12 @@
 angular.module('welc.controllers', [])
-    .controller('HomeCtrl', ['$scope', 'GameService', function ($scope, GameService) {
+    .controller('HomeCtrl', ['$scope', 'GameService', '$timeout', function ($scope, GameService, $timeout) {
         $scope.msg = 'Hello World';
         GameService.start();
-        GameService.pause();
+
+        //Will pause the game after 4100 milliseconds. Just an example of game service start and pause
+        $timeout(GameService.pause, 4100);
+
+        $timeout(GameService.start, 5100);
+
+        $timeout(GameService.pause, 9000);
     }]);
--- www/js/services/gameService.js
+++ www/js/services/gameService.js
@@ -2,11 +2,14 @@
  * Created by John on 5/05/2015.
  */
 angular.module('welc.services', [])
-    .service('GameService', [function () {
+    .service('GameService', ['$interval', function ($interval) {
         var running = false;
         var pollution = 0;  //Should be between 0 - 100
         var population = 100000; //Set initial population to 100, 000
         var powerPlants = []; //Contains all the power plants in the game
+
+        var stepInterval = 2000;    //Run game logic every 1000 milliseconds
+        var interval;   //This is the interval promise which will run the game
 
         this.addPowerPlant = function (powerPlant) {
             powerPlants.push(powerPlant);
@@ -25,12 +28,23 @@ angular.module('welc.services', [])
 
         this.start = function () {
             running = true;
-            console.log('Game is starting');
+            interval = $interval(stepGame, stepInterval);
+            console.log('Game is running');
         };
 
         this.pause = function () {
             running = false;
-            console.log('Game is pausing');
+            $interval.cancel(interval);
+            console.log('Game is paused');
+        };
+
+        /**
+         * This is where the game logic that should get run constantly goes.
+         * i.e pollution calculations
+         */
+        function stepGame() {
+            //Need some fancy algorithm here to calculate all the game state based on the games variables
+            console.log("Game calculating......");
         };
     }])
 ;
