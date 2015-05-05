c5ff738d4b6f0c306ac1b2593ea63793039a102e
John Armstrong <j.armstrong484@gmail.com>
2015-05-05 20:11:29 +1200

    Removed power plants from the game service and all functionality around them.

--- www/js/services/gameService.js
+++ www/js/services/gameService.js
@@ -6,32 +6,22 @@ angular.module('welc.services', [])
         var running = false;
         var pollution = 0;  //Should be between 0 - 100
         var population = 100000; //Set initial population to 100, 000
-        var powerPlants = []; //Contains all the power plants in the game
 
         var stepInterval = 2000;    //Run game logic every 1000 milliseconds
         var interval;   //This is the interval promise which will run the game
 
-        this.addPowerPlant = function (powerPlant) {
-            powerPlants.push(powerPlant);
-        };
-
-        this.removePowerPlant = function (powerPlant) {
-            var powerPlantIndex = powerPlant.indexOf(powerPlant);
-            if (powerPlantIndex > -1) {
-                powerPlants.splice(powerPlantIndex, 1);
-            }
-        };
-
-        this.getPowerPlants = function () {
-            return powerPlants;
-        };
-
+        /**
+         * Start running the game
+         */
         this.start = function () {
             running = true;
             interval = $interval(stepGame, stepInterval);
             console.log('Game is running');
         };
 
+        /**
+         * Pause the running of the game.
+         */
         this.pause = function () {
             running = false;
             $interval.cancel(interval);
