17a8070833e868c528ed1a8cfed4b868190514ac
Guest User <Guest@f-06-06-lab.mddn.vuw.ac.nz>
2015-05-15 13:49:11 +1200

    Drag and Drop. ngDragDrop breaks in app.js

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('welc', ['ionic', 'welc.controllers', 'welc.services', 'welc.directives'])
+angular.module('welc', ['ionic', 'welc.controllers', 'welc.services', 'welc.directives', 'ngDragDop'])
 
     .run(['$ionicPlatform', '$rootScope', '$state', function ($ionicPlatform, $rootScope, $state) {
         $ionicPlatform.ready(function () {
