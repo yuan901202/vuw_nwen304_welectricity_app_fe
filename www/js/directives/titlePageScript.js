6db55cb89c04a1f58fbd885f6e517a3f71457b45
John Armstrong <j.armstrong484@gmail.com>
2015-05-25 11:45:48 +1200

    Custom directive and js file use

---
+++ www/js/directives/titlePageScript.js
@@ -0,0 +1,12 @@
+/**
+ * Created by John on 25/05/2015.
+ */
+angular.module('welc.directives')
+    .directive('titlePageScript', function () {
+        return {
+            link: function link(scope, element, atttr) {
+                //All javascript for title page goes here
+
+            }
+        }
+    });
