44eaf657b143727733f01f2e4a15608897bc86a7
John Armstrong <j.armstrong484@gmail.com>
2015-05-26 11:01:28 +1200

    Have styling for dropped icons

---
+++ www/js/directives/plantDrop.js
@@ -0,0 +1,9 @@
+/**
+ * Created by John on 10/05/2015.
+ */
+angular.module('welc.directives')
+    .directive('plantDrop', function () {
+        return {
+            templateUrl: 'templates/plantDrop.html'
+        }
+    });
