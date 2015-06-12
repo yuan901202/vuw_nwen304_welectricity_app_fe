7caf9a3cf85f2a5c31ed11ffe44ca521aac51f2a
nicolaroundill <n.roundill@outlook.com>
2015-06-12 12:56:25 +1200

    Somewhat integrated my gameService code. Doesn't have ability to change some stats on power plant deletion.

--- www/js/controllers/gamePlayCtrl.js
+++ www/js/controllers/gamePlayCtrl.js
@@ -5,10 +5,12 @@ angular.module('welc.controllers')
     .controller('GamePlayCtrl', ['$scope', 'GameService', function ($scope, GameService) {
         //Static for now until service for loading them from server is created
         $scope.powerPlants = [
-            {name: 'Coal', image: 'img/powerPlants/Coal.png'},
-            {name: 'Wind', image: 'img/powerPlants/Wind.png'},
-            {name: 'Solar', image: 'img/powerPlants/Solar.png'},
-            {name: 'Hydro', image: 'img/powerPlants/Nuclear.png'}
+            {name: 'Coal', image: 'img/powerPlants/Coal.png', pollution: 4, energyOutput: 500, buyCost: 9900000},
+            {name: 'Wind', image: 'img/powerPlants/Wind.png', pollution: 1, energyOutput: 2, buyCost: 3020000},
+            {name: 'Solar', image: 'img/powerPlants/Solar.png', pollution: 1, energyOutput: 20, buyCost: 2790000},
+            {name: 'Hydro', image: 'img/powerPlants/Hydro.png', pollution: 1, energyOutput: 500, buyCost: 2660000},
+            {name: 'Nuclear', image: 'img/powerPlants/Nuclear.png', pollution: 1, energyOutput: 502, buyCost: 428000000},
+            {name: 'Oil', image: 'img/powerPlants/Oil.png', pollution: 4, energyOutput: 10, buyCost: 1710000}
         ];
 
         $scope.gamePlants = GameService.getPlants();
--- www/js/services/gameService.js
+++ www/js/services/gameService.js
@@ -4,8 +4,25 @@
 angular.module('welc.services')
     .service('GameService', ['$interval', function ($interval) {
         var running = false;
+        /*
         var pollution = 0;  //Should be between 0 - 100
         var population = 100000; //Set initial population to 100, 000
+        */
+
+        //-----Nicola's code start
+        var population = 210000;
+		var pollution = 0;
+		var tempPopulation;
+		var cost = 500000;
+		var tempCost;
+		var energy;
+		var energyBase = 100000;
+		var fakeEnergyOutput = 10500;
+		var tempEnergy;
+		var reqEnery;
+		var energyDiff;
+		var energyFactor;
+		//-----Nicola's code end
 
         var stepInterval = 2000;    //Run game logic every 1000 milliseconds
         var interval;   //This is the interval promise which will run the game
@@ -63,7 +80,74 @@ angular.module('welc.services')
          */
         function stepGame() {
             //Need some fancy algorithm here to calculate all the game state based on the games variables
+            //-----Nicola's code start
+            pollutionCount();
+            energyCount();
+			populationCount();
+			costCount();
+			update();
+			//-----Nicola's code end
             console.log("Game calculating......");
         };
+
+
+        //-----Nicola's code start
+        function pollutionCount(){
+			//pollution = (power plant array pollution code here)
+		}
+
+		function energyCount(){
+			//Energy Output
+		    //Energy increases based on:
+		    //Replace fakeEnergyOutput with the total energy output of power sources
+		    energy = energyBase + fakeEnergyOutput + (Math.random() * 100);
+		    //energy = energyBase + (POWER PLANT "energyOutput") + (Math.random() * 100);
+
+		    //Do we have enough energy?
+		    reqEnergy = (population * 0.5);
+		    //Calculate energy difference for calculating population
+		    energyDiff = energy - reqEnergy;
+		    //console.log("energy " + energy);
+		    //console.log("req energy " + reqEnergy);
+		    //console.log("energyDiff " + energyDiff);
+		}
+
+		function populationCount(){
+			//Population
+		    //Calculate the rate that the population changes by
+
+		    //If the energy difference is negative (not enough power)
+		    if(energyDiff < 0) {
+		    	//Population will decrease no matter what
+		    	tempPopulation = -(Math.random() * 50);
+		    } else {
+		    	//Population change = pollution level with a bit of random change
+		    	tempPopulation = -(pollution * 3) + (Math.random() * 100);
+		    }
+
+		    //Update Population
+			population += tempPopulation;
+			console.log("temp pop " + tempPopulation);
+
+		    //Make sure if population is ever a negative number it displays as 0
+			if (population < 0) { 
+				population = 0;
+			}
+		}
+
+		function costCount(){
+			//Cost
+		    tempCost = population * 0.1;
+		    cost += tempCost;
+		    console.log("cost " + cost);    
+		}		
+		//-----Nicola's code end
+
+
+
+
+
+
+
     }])
 ;
