713ab7607226f62491fff22d2de2ff8d79b6741d
John Armstrong <j.armstrong484@gmail.com>
2015-06-20 20:17:01 +1200

    Added ngAudio library to play sound. Will play a small blop sound when a plant is dropped

--- www/js/app.js
+++ www/js/app.js
@@ -8,7 +8,7 @@ angular.module('welc.directives', []);
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('welc', ['ionic', 'welc.controllers', 'welc.services', 'welc.directives', 'ngDragDrop'])
+angular.module('welc', ['ionic', 'welc.controllers', 'welc.services', 'welc.directives', 'ngDragDrop', 'ngAudio'])
     .run(['$ionicPlatform', '$rootScope', '$state', '$ionicLoading', '$timeout', 'GameService', 'saveLoadService', 'userInfoService',
         function ($ionicPlatform, $rootScope, $state, $ionicLoading, $timeout, GameService, saveLoadService, userInfoService) {
         $ionicPlatform.ready(function () {
